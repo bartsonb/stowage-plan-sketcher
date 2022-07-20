6af486c581ac0703ce2993be98207d9b5bc19a99
Bartsonb <armin.bartnik@gmail.com>
2022-07-20 23:52:34 +0200

    Added new icons to MenuBar

--- src/Components/MenuBar/MenuBar.tsx
+++ src/Components/MenuBar/MenuBar.tsx
@@ -1,17 +1,31 @@
 import "./MenuBar.scss";
 
+import createIcon from '../../Assets/Icons/create.svg';
+import saveIcon from '../../Assets/Icons/save.svg';
+import exportIcon from '../../Assets/Icons/export.svg';
+
 export interface MenuBarProps {
     timestampLastSave: number;
-    changesMade: boolean
+    changesMade: boolean;
 }
 
 export const MenuBar = (props: MenuBarProps) => {
     const { timestampLastSave, changesMade } = props;
-    
+
     return (
         <div className="MenuBar">
-            <div className="MenuBar__Button MenuBar__Button--create">Create new sketch</div>
-            <div className="MenuBar__Button MenuBar__Button--save">Save sketch<span>(Saved last {timestampLastSave}s ago)</span></div>
+            <div className="MenuBar__Button MenuBar__Button--create">
+                <img src={createIcon} alt="" />
+                Create new sketch
+            </div>
+            <div className="MenuBar__Button MenuBar__Button--save">
+                <img src={saveIcon} alt="" />
+                Save sketch<span>(Saved last {timestampLastSave}s ago)</span>
+            </div>
+            <div className="MenuBar__Button MenuBar__Button--export">
+                <img src={exportIcon} alt="" />
+                Export sketch
+            </div>
         </div>
     );
 };
