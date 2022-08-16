1bf105082df34b9ab856eb595bf79c61cf90c2e0
Bartsonb <armin.bartnik@gmail.com>
2022-08-16 03:25:43 +0200

    Moved Header from Layout to Component

--- client/src/Layouts/MainLayout/MainLayout.tsx
+++ client/src/Layouts/MainLayout/MainLayout.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Header from "../Header/Header";
+import Header from "../../Components/Header/Header";
 import { User } from "../../App";
 import "./MainLayout.scss";
 
